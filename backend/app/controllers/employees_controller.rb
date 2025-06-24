a5fd91b6583c300ea4aac28f2c8548072876866c
jabezinu <jabezzinu31@gmail.com>
2025-06-24 12:41:39 +0300

    employees controller

--- backend/app/controllers/employees_controller.rb
+++ backend/app/controllers/employees_controller.rb
@@ -1,2 +1,41 @@
 class EmployeesController < ApplicationController
+  def index
+    employees = Employee.all
+    render json: employees
+  end
+
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee
+  end
+
+  def create
+    employee = Employee.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    employee = Employee.find(params[:id])
+    if employee.update(employee_params)
+      render json: employee
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    employee = Employee.find(params[:id])
+    employee.destroy
+    head :no_content
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :phone, :position, :image, :date_hired, :salary)
+  end
 end

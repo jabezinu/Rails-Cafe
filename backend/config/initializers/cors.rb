2a27d2988d221604886c053ba7bff6e73a8ad37e
jabezinu <jabezzinu31@gmail.com>
2025-06-28 03:16:38 +0300

    include all origin

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "*" 
+    # you can allow all 
 
     resource "*",
       headers: :any,
